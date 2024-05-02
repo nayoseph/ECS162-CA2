201aa554e82431cfbb05a6a07b27bd7e265e9e60
nayoseph <nayoseph@ucdavis.edu>
2024-05-01 23:56:19 -0700

    bug fixes, 3x3 validation
    completed 3x3 validation, clearing individual cell bug fix, game over screen, board loading bug fix

--- dashboard/sudoku/sudoku.js
+++ dashboard/sudoku/sudoku.js
@@ -4,20 +4,25 @@ let selectedBoardIndex = null;
 
 const boardCells = document.querySelectorAll('.cell');
 const keyPadCells = document.querySelectorAll('.number-pad-cell');
-let solvedSudoku = [5, 3, 4, 6, 7, 8, 9, 1, 2, 
-	6, 7, 2, 1, 9, 5, 3, 4, 8, 
-	1, 9, 8, 3, 4, 2, 5, 6, 7, 
-	8, 5, 9, 7, 6, 1, 4, 2, 3, 
-	4, 2, 6, 8, 5, 3, 7, 9, 1, 
-	7, 1, 3, 9, 2, 4, 8, 5, 6, 
-	9, 6, 1, 5, 3, 7, 2, 8, 4, 
-	2, 8, 7, 4, 1, 9, 6, 3, 5, 
-	3, 4, 5, 2, 8, 6, 1, 7, 9];
+let sudokuBoard = [
+	[5, 3, 4, 6, 7, 8, 9, 1, 2],
+	[6, 7, 2, 1, 9, 5, 3, 4, 8],
+	[1, 9, 8, 3, 4, 2, 5, 6, 7],
+	[8, 5, 9, 7, 6, 1, 4, 2, 3],
+	[4, 2, 6, 8, 5, 3, 7, 9, 1],
+	[7, 1, 3, 9, 2, 4, 8, 5, 6],
+	[9, 6, 1, 5, 3, 7, 2, 8, 4],
+	[2, 8, 7, 4, 1, 9, 6, 3, 5],
+	[3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ];
+  
+// Flatten the 2D array into a 1D array
+let solvedSudoku = sudokuBoard.flat();
 
-remove60(solvedSudoku);
+remove35(solvedSudoku);
 
 function isGameOver() {
-	for (let i = 1; i < 82; ++i) {
+	for (let i = 1; i <= 81; ++i) {
 		let cell = document.getElementById("cell-"+i);
 		if(cell.textContent === "") {
 			return false;
@@ -26,10 +31,10 @@ function isGameOver() {
 	return true;
 }
 
-function remove60(board) {
+function remove35(board) {
 	let count = 0;
-	while(count < 50) {
-		let randomCell = Math.floor((Math.random() * 80) + 1);
+	while(count < 35) {
+		let randomCell = Math.floor((Math.random() * 80));
 		if(board[randomCell] != "") {
 			board[randomCell] = "";
 			++count;
@@ -38,13 +43,13 @@ function remove60(board) {
 }
 
 function loadBoard(board) {
-	for(let i = 1; i < 82; ++i) {
-		if(board[i] == "") {
+	for(let i = 1; i <= 81; ++i) {
+		if(board[i - 1] == "") {
 			continue;
 		}
 		const cell = document.getElementById("cell-" + i);
 		cell.setAttribute("removable", "false");
-		cell.textContent = board[i];
+		cell.textContent = board[i - 1];
 	}
 }
 
@@ -57,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function createBoard() {
     let board = document.getElementById("board");
-    for(let i = 1; i < 82; i++) {
+    for(let i = 1; i <= 81; i++) {
         const cell = document.createElement('button');
         cell.className = "cell";
         cell.id = "cell-" + i;
@@ -92,31 +97,53 @@ function attachEventListeners() {
 }
 
 function handleBoardCellClick(boardCellClickEvent) {
+	if(!gameActive) {
+		return;
+	}
 	console.log("board click");
 	const clickedBoardCell = boardCellClickEvent.target;
+	console.log(clickedBoardCell);
     const clickedCellIndex = parseInt(clickedBoardCell.getAttribute('index'));
 	const displayText = document.getElementById('display');
 
 	if(selectedKeyIndex == null && clickedBoardCell.textContent == "") {
 		displayText.textContent = "You must select a number before selecting a cell";
 		return;
-	} else if(selectedKeyIndex == null && clickedBoardCell.textContent != "" && clickedBoardCell.getAttribute("removable") != false) {
+	} else if(selectedKeyIndex == null && clickedBoardCell.textContent != "" && clickedBoardCell.getAttribute("removable") != "false") {
 		clickedBoardCell.textContent = "";
-	}else if(clickedBoardCell.textContent != "") {
+		return;
+	} else if(clickedBoardCell.textContent != "") {
 		displayText.textContent = "You must select an empty cell";
 		return;
 	} else if(validColumn(clickedCellIndex, selectedKeyIndex) && validRow(clickedCellIndex, selectedKeyIndex) && valid3by3Section(clickedCellIndex, selectedKeyIndex)) {
 		clickedBoardCell.textContent = "" + selectedKeyIndex;
 		displayText.textContent = "";
-	} else {
-		displayText.textContent = "The number you selected breaks the rules of Sudoku.";
 	}
+
+	if(isGameOver()) {
+		gameActive = false;
+		displayText.textContent = "Congrats! You won!";
+	}
+
 	selectedKeyIndex = null;
 	selectedBoardIndex = null;
 }
 
 function valid3by3Section(boardCellIndex, keyIndex) {
-	// TODO: FIX
+	let startRowIndex = Math.floor(Math.floor(boardCellIndex / 9) / 3) * 3;
+	let startColIndex = Math.floor(((boardCellIndex - 1) % 9) / 3) * 3;
+	const displayText = document.getElementById('display');
+
+	for(let i = 0; i < 3; ++i) {
+		for(let j = 0; j < 3; ++j) {
+			let ind = (startRowIndex + i) * 9 + (startColIndex + j) + 1;
+			console.log(ind);
+			if(parseInt(document.getElementById('cell-' + ind).textContent) == keyIndex) {
+				displayText.textContent = "You can't have multiple of the same number in a 3x3 block";
+				return false;
+			}
+		}
+	}
 	return true;
 }
 
@@ -125,7 +152,7 @@ function validColumn(boardCellIndex, keyIndex) {
 	let startIndex = boardCellIndex % 9;
 	let displayText = document.getElementById('display');
 
-	for(let i = startIndex; i < 82; i += 9) {
+	for(let i = startIndex; i <= 81; i += 9) {
 		if(parseInt(document.getElementById('cell-' + i).textContent) == keyIndex) {
 			displayText.textContent = "You can't have multiple of the same number in a column";
 			return false;
@@ -136,10 +163,10 @@ function validColumn(boardCellIndex, keyIndex) {
 
 function validRow(boardCellIndex, keyIndex) {
 	// TODO: FIX
-	let startIndex = Math.floor(boardCellIndex / 9) * 9 + 1;
+	let startIndex = Math.floor(boardCellIndex / 9) * 9;
 	let displayText = document.getElementById('display');
 
-	for(let i = 0; i < 9; ++i) {
+	for(let i = 1; i <= 8; ++i) {
 		if(parseInt(document.getElementById('cell-' + (i + startIndex)).textContent) == keyIndex) {
 			displayText.textContent = "You can't have multiple of the same number in a row";
 			return false;
@@ -150,7 +177,10 @@ function validRow(boardCellIndex, keyIndex) {
 }
 
 function handleNumberPadCellClick(padCellClickEvent) {
-	console.log("board click");
+	if(!gameActive) {
+		return;
+	}
+	console.log("pad click");
 	const clickedKeyPadCell = padCellClickEvent.target;
     const clickedKeyPadIndex = parseInt(clickedKeyPadCell.getAttribute('index'));
 	selectedKeyIndex = clickedKeyPadIndex;
@@ -160,9 +190,14 @@ function handleNumberPadCellClick(padCellClickEvent) {
 
 function restartGame() {
 	clearBoard();
+	loadBoard(solvedSudoku);
+	gameActive = true;
 	selectedBoardIndex = null;
 	selectedKeyIndex = null;
 	console.log("game restarted");
+	let displayText = document.getElementById('display');
+
+	displayText.textContent = "Good Luck!";
 }
 
 function clearBoard() {
